6e0be2912ac0d9d0222e06f5adbd6f6d8a8b84a4
Arushi Rastogi <arushirastogi@Arushis-MacBook-Pro.local>
2020-06-15 18:19:01 -0400

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,51 @@
+console.log('Client side javascript is loaded')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+// fetch('http://localhost:3000/weather?address=!').then((response)=>{
+//     response.json().then((data) => {
+//         if(data.error){
+//             console.log(data.error)
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    messageOne.textContent = 'Loading..'
+    messageTwo.textContent = '' 
+
+    fetch('http://localhost:3000/weather?address=' + search.value).then((response)=>{
+    response.json().then((data) => {
+        if(data.error){
+            //console.log(data.error)
+            messageOne.textContent = data.error
+            messageTwo.textContent = ''
+        } else {
+            //console.log(data.location)
+            //console.log(data.forecast)
+            messageOne.textContent = data.location,
+            messageTwo.textContent = data.forecast
+        }
+    })
+})
+
+    //const location = search.value
+
+    //console.log(location)
+})
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,32 @@
+//const url = 'http://api.weatherstack.com/current?access_key=11199071f3bf755946687fd1325660d8&query=37.8267,-122.4233&units=f'
+
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to the weather service')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike +' degrees.')
+//     }
+//     // const data = JSON.parse(response.body)
+//     // console.log(data.current)
+//     //console.log(response)
+//     //console.log(response.body.current)
+// })
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=11199071f3bf755946687fd1325660d8&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({url, json:true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather services!', undefined)
+        } else if (body.error) {
+            callback('Unable to find weather description. Try another search.', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike +' degrees.')
+        }
+    })
+}
+
+module.exports = forecast
